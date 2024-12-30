fca983ee61a3be424eb47405dc57e9fe06a84261
rahulrajdahal <rahulrajdahal@gmail.com>
2024-12-30 15:53:23 +0000

    new initial

--- lib/ui/styles.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-abstract class AppColors {
-  static const Color kprimaryColor = Color(0xffF1F3F6);
-  static const Color ksecondaryColor = Color(0xff0150BC);
-  static const Color ktextColor = Color(0xff707070);
-  static const Color ktextFieldColor = Color(0xffDEDFE1);
-}
